4db228f363ea2a5589c0e2f0a666ca05c1dd153e
DaDevMate <67144384+dev365b@users.noreply.github.com>
2023-03-13 11:20:54 +0800

    Initial Commit

---
+++ HomeworkGen/HomeworkGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeworkGenApp.swift
+//  HomeworkGen
+//
+//  Created by Nethan on 13/3/23.
+//
+
+import SwiftUI
+
+@main
+struct HomeworkGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
